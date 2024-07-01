8b357af38befdc3d131305ef413cf5830b12bd50
Krisha Thumar <krishathumar2308@gmail.com>
2024-07-01 17:28:11 +0530

    url changes

--- shipmnt-app/src/url/baseurl.js
+++ shipmnt-app/src/url/baseurl.js
@@ -1,2 +1,2 @@
-// export const baseurl="https://shipmentplacement-backend.onrender.com";
-export const baseurl="http://localhost:8000";
+export const baseurl="https://shipmentplacement-backend.onrender.com";
+// export const baseurl="http://localhost:8000";
